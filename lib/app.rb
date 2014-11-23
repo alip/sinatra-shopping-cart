6ff1b6e5f7c11db32ad3dccb9296444985e6d2df
Ali Polatel <polatel@gmail.com>
2014-11-23 14:11:11 +0200

    setup swagger blocks

--- lib/app.rb
+++ lib/app.rb
@@ -4,15 +4,53 @@
 
 require 'sinatra/base'
 require 'sinatra/activerecord'
+require 'swagger/blocks'
 
 require_relative '../models/init'
 
 class SampleShop < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
-  set :app_file, __FILE__
+  configure do
+    set :app_file, __FILE__
+  end
+
+  configure :production do
+    BASE_PATH = 'http://188.226.149.34/api'
+    set :raise_errors, false
+    set :show_exceptions, false
+  end
+
+  configure :development, :test do
+    BASE_PATH = 'http://localhost:4567/api'
+    enable :logging, :dump_errors, :raise_errors
+  end
+
+  include Swagger::Blocks
+  SWAGGERED_CLASSES = [User, SampleShop].freeze
+
+  swagger_root do
+    key :swaggerVersion, '1.2'
+    key :apiVersion, '0.1'
+    key :basePath, BASE_PATH
+    info do
+      key :title, 'Sinatra Shopping Cart API Sample'
+    end
+  end
 
   get '/' do
     redirect '/index.html' # Swagger
+  end
+
+  before '/api' do
+    content_type :json
+  end
+
+  get '/api/api-docs' do
+    Swagger::Blocks.build_root_json(SWAGGERED_CLASSES).to_json
+  end
+
+  get '/api/api-docs/:path' do
+    Swagger::Blocks.build_api_json(params[:path], SWAGGERED_CLASSES).to_json
   end
 end
--- models/user.rb
+++ models/user.rb
@@ -4,4 +4,26 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, :uniqueness => true
 
   has_secure_password
+
+  include Swagger::Blocks
+  swagger_model :User do
+    key :id, :User
+    property :id do
+      key :type, :integer
+      key :format, :int64
+      key :required, :true
+    end
+    property :name do
+      key :type, :string
+      key :required, :true
+    end
+    property :username do
+      key :type, :string
+      key :required, :true
+    end
+    property :name do
+      key :type, :string
+      key :required, :true
+    end
+  end
 end
